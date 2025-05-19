4df334c2b23cd578c96997b6b7c60d9534124753
Nikola Davidovac <davidovacnikola@gmail.com>
2025-05-20 01:47:00 +0200

    Dodata metoda u clanstva kontroler za sortiranje korisnika po grupi(A)

--- DrustvenaMreza/Controllers/ClanstvaController.cs
+++ DrustvenaMreza/Controllers/ClanstvaController.cs
@@ -9,6 +9,27 @@ namespace DrustvenaMreza.Controllers
     [ApiController]
     public class ClanstvaController : ControllerBase
     {
-        
+        private RepositoryGrupe repositoryGrupe = new RepositoryGrupe();
+        private RepositoryKorisnici repositoryKorisnici = new RepositoryKorisnici();
+        private RepositoryClanstva repositoryClanstva = new RepositoryClanstva();
+
+        // GET: api/clanstva/{id}
+        [HttpGet]
+        public ActionResult<string> GetAllUsersByGroup([FromQuery] int groupId)
+        {
+            if (!RepositoryGrupe.Data.ContainsKey(groupId))
+            {
+                return NotFound();
+            }
+            List<Korisnik> korisnici = new List<Korisnik>();
+            foreach (var clanstvo in RepositoryClanstva.Data.Values)
+            {
+                if (clanstvo.Grupa.Id == groupId)
+                {
+                    korisnici.Add(clanstvo.Korisnik);
+                }
+            }
+            return Ok(korisnici);
+        }
     }
 }
